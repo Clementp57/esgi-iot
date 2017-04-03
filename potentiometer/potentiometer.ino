7a69ab978738641976cb356a5bb4d5345fcf71b2
Damien Bannerot <bannerotdamien@gmail.com>
2017-04-03 16:54:57 +0200

    add inverted led

--- potentiometer/potentiometer.ino
+++ potentiometer/potentiometer.ino
@@ -1,17 +1,26 @@
 int ledPin = 3;
+int ledPinInvert = 5;
 int potentiometerAnalogPin = 0;
 int val = 0;
+int output = 0;
+int outputInvert = 0;
 
 void setup() {
   // put your setup code here, to run once:
   pinMode(ledPin, OUTPUT);
+  pinMode(ledPinInvert, OUTPUT);
   Serial.begin(9600);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
   val = analogRead(potentiometerAnalogPin);
-  Serial.print(val*254/1024);
+  output = val*254/1024;
+  outputInvert = 255-val*254/1024;
+  Serial.print(output);
   Serial.print("\n");
-  analogWrite(ledPin, val*254/1024);
+  Serial.print(outputInvert);
+  Serial.print("\n");
+  analogWrite(ledPin, output);
+  analogWrite(ledPinInvert, outputInvert);
 }
