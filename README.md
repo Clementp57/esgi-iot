5724c5c1dcce186338156743a93820d79947e187
Clement Peyrabere <clementpeyrabere@me.com>
2017-04-03 14:05:47 +0200

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # esgi-iot
+
+
+# URL to for board download
+https://raw.githubusercontent.com/stm32duino/BoardManagerFiles/master/STM32/package_stm_index.json
---
+++ sketch_apr03a/sketch_apr03a.ino
@@ -0,0 +1,16 @@
+int ledPin = 9;
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(LED_BUILTIN, OUTPUT);
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+   digitalWrite(ledPin, HIGH);
+   delay(3000);
+   digitalWrite(ledPin, LOW);
+   delay(3000);
+}
