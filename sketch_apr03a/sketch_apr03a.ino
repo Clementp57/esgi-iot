1c262537d8cb57fa5e99939341bdb3c5b0babe1b
Clement Peyrabere <clementpeyrabere@me.com>
2017-04-03 16:26:56 +0200

    feux tricolores

--- sketch_apr03a/sketch_apr03a.ino
+++ sketch_apr03a/sketch_apr03a.ino
@@ -1,25 +1,38 @@
-int redLedPin = 7;
-int orangeLedPin = 8;
-int greenLedPin = 9;
+int redLedPin1 = 7;
+int orangeLedPin1 = 8;
+int greenLedPin1 = 9;
+
+int redLedPin2 = 4;
+int orangeLedPin2 = 5;
+int greenLedPin2 = 6;
 
 void setup() {
   // put your setup code here, to run once:
-  pinMode(redLedPin, OUTPUT);
-  pinMode(orangeLedPin, OUTPUT);
-
+  pinMode(redLedPin1, OUTPUT);
+  pinMode(orangeLedPin1, OUTPUT);
+  pinMode(greenLedPin1, OUTPUT);
+  pinMode(redLedPin2, OUTPUT);
+  pinMode(orangeLedPin2, OUTPUT);
+  pinMode(greenLedPin2, OUTPUT);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  digitalWrite(redLedPin, HIGH);
-  delay(5000);
-  digitalWrite(redLedPin, LOW);
-
-  digitalWrite(orangeLedPin, HIGH); 
+  digitalWrite(redLedPin1, HIGH);
+  digitalWrite(greenLedPin2, HIGH);
+  delay(3000);
+  digitalWrite(greenLedPin2, LOW);
+  digitalWrite(orangeLedPin2, HIGH);
   delay(1000);
-  digitalWrite(orangeLedPin, LOW);
-  
-  digitalWrite(greenLedPin, HIGH);
-  delay(5000); 
-  digitalWrite(greenLedPin, LOW);
+  digitalWrite(orangeLedPin2, LOW);
+  digitalWrite(redLedPin2, HIGH);
+  digitalWrite(redLedPin1, LOW);
+  digitalWrite(greenLedPin1, HIGH);
+  delay(3000);
+  digitalWrite(greenLedPin1, LOW);
+  digitalWrite(orangeLedPin1, HIGH);
+  delay(1000);
+  digitalWrite(orangeLedPin1, LOW);
+  digitalWrite(redLedPin2, LOW);
+
 }
