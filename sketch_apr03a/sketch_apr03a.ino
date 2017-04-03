dc5f159f814be4e6877ddc54af652c8ad642c118
Clement Peyrabere <clementpeyrabere@me.com>
2017-04-03 14:39:59 +0200

    3 leds g/o/r

--- sketch_apr03a/sketch_apr03a.ino
+++ sketch_apr03a/sketch_apr03a.ino
@@ -1,16 +1,25 @@
-int ledPin = 9;
+int redLedPin = 7;
+int orangeLedPin = 8;
+int greenLedPin = 9;
 
 void setup() {
   // put your setup code here, to run once:
-  pinMode(LED_BUILTIN, OUTPUT);
+  pinMode(redLedPin, OUTPUT);
+  pinMode(orangeLedPin, OUTPUT);
 
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+  digitalWrite(redLedPin, HIGH);
+  delay(5000);
+  digitalWrite(redLedPin, LOW);
 
-   digitalWrite(ledPin, HIGH);
-   delay(3000);
-   digitalWrite(ledPin, LOW);
-   delay(3000);
+  digitalWrite(orangeLedPin, HIGH); 
+  delay(1000);
+  digitalWrite(orangeLedPin, LOW);
+  
+  digitalWrite(greenLedPin, HIGH);
+  delay(5000); 
+  digitalWrite(greenLedPin, LOW);
 }
