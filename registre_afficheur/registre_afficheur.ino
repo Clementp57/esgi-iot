68f04780dcc68137a212c0c04dbd20bbb03330cb
Damien Bannerot <bannerotdamien@gmail.com>
2017-04-05 12:58:40 +0200

    add values to dice (6 to 16, hexa)

--- registre_afficheur/registre_afficheur.ino
+++ registre_afficheur/registre_afficheur.ino
@@ -8,12 +8,24 @@ int buttonPin = 2;
 
 boolean registers[numOfRegisterPins];
 
-boolean dice[6][7] = {{LOW,HIGH,HIGH,LOW,LOW,LOW,LOW},
-{HIGH,HIGH,LOW,HIGH,HIGH,LOW,HIGH},
-{HIGH,HIGH,HIGH,HIGH,LOW,LOW,HIGH},
-{LOW,HIGH,HIGH,LOW,LOW,HIGH,HIGH},
-{HIGH,LOW,HIGH,HIGH,LOW,HIGH,HIGH},
-{HIGH,LOW,HIGH,HIGH,HIGH,HIGH,HIGH}};
+boolean dice[16][7] = {
+  {HIGH,HIGH,HIGH,HIGH,HIGH,HIGH,LOW},//0
+  {LOW,HIGH,HIGH,LOW,LOW,LOW,LOW},//1
+  {HIGH,HIGH,LOW,HIGH,HIGH,LOW,HIGH},//2
+  {HIGH,HIGH,HIGH,HIGH,LOW,LOW,HIGH},//3
+  {LOW,HIGH,HIGH,LOW,LOW,HIGH,HIGH},//4
+  {HIGH,LOW,HIGH,HIGH,LOW,HIGH,HIGH},//5
+  {HIGH,LOW,HIGH,HIGH,HIGH,HIGH,HIGH},//6
+  {HIGH,HIGH,HIGH,LOW,LOW,LOW,LOW},//7
+  {HIGH,HIGH,HIGH,HIGH,HIGH,HIGH,HIGH},//8
+  {HIGH,HIGH,HIGH,HIGH,LOW,HIGH,HIGH},//9
+  {HIGH,HIGH,HIGH,LOW,HIGH,HIGH,HIGH},//A
+  {LOW,LOW,HIGH,HIGH,HIGH,HIGH,HIGH},//B
+  {HIGH,LOW,LOW,HIGH,HIGH,HIGH,LOW},//C
+  {LOW,HIGH,HIGH,HIGH,HIGH,LOW,HIGH},//D
+  {HIGH,LOW,LOW,HIGH,HIGH,HIGH,HIGH},//E
+  {HIGH,LOW,LOW,LOW,HIGH,HIGH,HIGH},//F
+  };
 
 void setup() {
   Serial.begin(9600);
@@ -34,7 +46,7 @@ void loop() {
 }
 
 void randomDice() {
-  int diceNum = random(1,6);
+  int diceNum = random(0,16);
   for(int i = 1;i<=7;i++) {
     setRegisterPin(i, dice[diceNum][i-1]);
   }
